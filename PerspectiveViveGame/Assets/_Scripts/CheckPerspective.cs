4e7b8426195368a28b60cd676862f88b4aad86f3
xeliot <daveho8888@gmail.com>
2017-08-01 23:16:21 -0400

    initial commit

---
+++ PerspectiveViveGame/Assets/_Scripts/CheckPerspective.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPerspective : MonoBehaviour {
+
+	private bool correctPos;
+	// Use this for initialization
+	void Start () {
+		correctPos = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var vrcam = SteamVR_Render.Top();
+		Ray ray = new Ray (vrcam.head.position, vrcam.head.forward);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 1000)) {
+			if (hit.collider.tag == "focus" && correctPos) {
+				Debug.Log ("Key found!");
+			}
+			else if (hit.collider.tag == "focus") {
+				Debug.Log ("Gaze is correct, but position is wrong");
+			}
+		}
+
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "posBox") {
+			correctPos = true;
+		}
+	}
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "posBox") {
+			correctPos = false;
+		}
+	}
+
+}
---
+++ PerspectiveViveGame/Assets/_Scripts/MenuScene.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScene : MonoBehaviour {
+
+    [SerializeField]
+    private Button startButton;
+
+    [SerializeField]
+    private Button exitButton;
+
+	// Use this for initialization
+	void Start () {
+        Button start = startButton.GetComponent<Button>();
+        start.onClick.AddListener(startGame);
+
+        Button exit = exitButton.GetComponent<Button>();
+        exit.onClick.AddListener(exitGame);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void startGame() {
+        Debug.Log("the start button has been clicked");
+        SceneManager.LoadScene("FirstLevel");
+    }
+
+    private void exitGame() {
+        Debug.Log("game has been exited");
+        Application.Quit();
+    }
+}
